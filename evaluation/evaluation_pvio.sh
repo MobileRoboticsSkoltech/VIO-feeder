47310bd5b6dfe192ac82f86bf0ca879cf5044828
Hekmat Taherinejad <Hekmat.Taherinejad@skoltech.ru>
2021-10-21 17:24:07 +0300

    little changes

--- evaluation/evaluation_pvio.sh
+++ evaluation/evaluation_pvio.sh
@@ -1,4 +1,5 @@
-#!/bin/bash/
+#!/bin/bash
 # A change 
 
-../methods/PVIO/build/pvio-pc/pvio-pc tum:///../datasets/tumvi_data/dataset-room1_512_16/mav0 ../config/tum-vi.yaml
+../methods/PVIO/build/pvio-pc/pvio-pc tum:///../datasets/tumvi_data/dataset-room1_512_16/mav0 ../methods/PVIO/config/tum-vi.yaml
+
